c3ccfa32b7ef0440b86947196ccf1627eb429991
nondotxyz <nondotxyz@users.noreply.github.com>
2023-03-21 20:50:16 +0700

    fix: using codicons

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,7 +3,39 @@ local spec = {
 	dependencies = {
 		{ 'kyazdani42/nvim-web-devicons', lazy = true },
 		{ 'arkav/lualine-lsp-progress' },
-		{ 'SmiteshP/nvim-navic' }
+		{
+			'SmiteshP/nvim-navic',
+			config = {
+				icons = {
+					File = ' ',
+					Module = ' ',
+					Namespace = ' ',
+					Package = ' ',
+					Class = ' ',
+					Method = ' ',
+					Property = ' ',
+					Field = ' ',
+					Constructor = ' ',
+					Enum = ' ',
+					Interface = ' ',
+					Function = ' ',
+					Variable = ' ',
+					Constant = ' ',
+					String = ' ',
+					Number = ' ',
+					Boolean = ' ',
+					Array = ' ',
+					Object = ' ',
+					Key = ' ',
+					Null = ' ',
+					EnumMember = ' ',
+					Struct = ' ',
+					Event = ' ',
+					Operator = ' ',
+					TypeParameter = ' '
+				}
+			}
+		}
 	}
 }
 
